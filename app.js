c49abb8c8492ad6c3a565da153b2091450515f2d
Wendy <lienyun.chang@gmail.com>
2022-05-11 13:36:44 +0800

    cowsay

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
-//連接hello.js
+//內建模組
+const path = require('path')
+const http = require('http')
+
+//第三方套件
+const cowsay = require('cowsay');
+//自建模組
+
 const hello = require('./hello')
 
-//呼叫hello的
-hello.helloModule()
+//////////////////////////////////
 
-console.log(hello.title)
+
+let sentences = ['Kelly~~', '早日', '康復哞哞哞'];
+
+sentences.forEach((sentence) => {
+    console.log(cowsay.say({
+        text : sentence,
+        e : "^^",
+        T : "U "
+    }));
+});
