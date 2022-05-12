4e0b7f34b6e7f11b702af56ee0fd01c57f88688a
Wendy <lienyun.chang@gmail.com>
2022-05-12 11:07:51 +0800

    form

--- app.js
+++ app.js
@@ -2,36 +2,48 @@
 const path = require('path')
 //第三方套件
 const express = require('express')
+const bodyParser = require('body-parser');
+    //解析console.log undefined的部分
 //自建模組
 
 //////////////////////////////////
 const app = express()
 
-//middleware 介於req and res中間
-
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+    //解析console.log undefined的部分
 
 app.use((req, res, next) => {
 	console.log('Hello!');
     next();
 });
-//沒有next();的話，就只會印出Hello
+
 app.use((req, res, next) => {
 	console.log('World!');
     next();
 });
 
-//處理get請求(瀏覽器)
 app.get('/', (req, res) => {
-    //用原生方式寫，很複雜↓
-    // res.writeHead(200, { 'Content-Type': 'text/html' });
-    // res.write('<head><meta charset="utf-8" /></head>')
-    // res.write('<body>')
-    // res.write('<h1>這是首頁</h1>')
-    // res.write('</body>')
-    //使用express寫↓可以直接連接html印在網頁上
     res.status(200).sendFile(path.join(__dirname,'views','index.html'))
 });
 
+
+
+app.get('/login', (req, res) => {
+    res.status(200).sendFile(path.join(__dirname,'views','login.html'))
+});
+
+app.post('/login', (req, res) => {
+    console.log('login data', req.body);
+    //這邊要安裝parse才能解析 不然會undefined
+    const { email, password } = req.body;
+    if (email && password) {
+        res.redirect('/');
+        //如果有填寫email跟密碼，就回首頁
+    } else {
+				console.log('欄位尚未填寫完成！')
+    }
+});
 
 app.listen(3000, () => {
 	console.log('Web Server is running on port 3000');
