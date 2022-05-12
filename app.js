4743e6d94a4370e3f1142f4886e1c4602e1a7372
Wendy <lienyun.chang@gmail.com>
2022-05-12 14:42:27 +0800

    EJS include 使用模板片段

--- app.js
+++ app.js
@@ -17,17 +17,7 @@ app.set('views', 'views');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
-    //解析console.log undefined的部分
 
-app.use((req, res, next) => {
-	console.log('Hello!');
-    next();
-});
-
-app.use((req, res, next) => {
-	console.log('World!');
-    next();
-});
 
 app.get('/', (req, res) => {
     res.status(200)
